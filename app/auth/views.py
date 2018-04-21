3b7087e17918f71ff8013e92f3dba420d88e2b00
lifan <williamlifan@163.com>
2018-04-21 18:04:31 +0800

    Add auth module

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,44 @@
 # -*- coding: utf-8 -*-
 
 from . import auth
-from flask import render_template
+from .. import db
+from .forms import LoginForm, RegistrationForm
+from ..models import User
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_required, login_user, logout_user
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('auth/login.html')
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user, form.remember_me.data)
+			next = request.args.get('next')
+			if next is None or not next.startswith('/'):
+				next = url_for('main.index')
+			return redirect(next)
+		flash('Invalid username or password.')
+	return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('You have been logged out.')
+	return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data,
+					username=form.username.data,
+					password=form.password.data)
+		db.session.add(user)
+		flash('You can now login.')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
 	name = StringField(label='What is your name?', validators=[Required()])
 	submit = SubmitField(label='Submit')
