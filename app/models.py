4651c29e597e855b4ed4ca9f6c97701eb191a26e
lifan <williamlifan@163.com>
2018-04-23 09:33:16 +0800

    Add permissions to Role

--- app/models.py
+++ app/models.py
@@ -3,19 +3,45 @@
 from . import db
 from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+
+class Permissions(object):
+	FOLLOW = 0x01
+	COMMENT = 0x02
+	WRITE_ARTICLES = 0x04
+	MODERATE_COMMENTS = 0x08
+	ADMINISTER = 0x80
 
 
 class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean, default=False, index=True)
+	permissions = db.Column(db.Integer)
 	users = db.relationship('User', backref='role')
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
+
+	@staticmethod
+	def insert_roles():
+		roles = {
+			'User': (Permissions.FOLLOW | Permissions.COMMENT | Permissions.WRITE_ARTICLES, True),
+			'Moderator': (Permissions.FOLLOW | Permissions.COMMENT | Permissions.WRITE_ARTICLES | Permissions.MODERATE_COMMENTS, False),
+			'Administrator': (0xff, False)
+		}
+		for r in roles:
+			role = Role.query.filter_by(name=r).first()
+			if role is None:
+				role = Role(name=r)
+			role.permissions = roles[r][0]
+			role.default = roles[r][1]
+			db.session.add(role)
+		db.session.commit()
 
 
 class User(UserMixin, db.Model):
@@ -29,6 +55,20 @@ class User(UserMixin, db.Model):
 
 	def __repr__(self):
 		return '<User %r>' % self.username
+	
+	def __init__(self, **kwargs):
+		super(User, self).__init__(**kwargs)
+		if self.role is None:
+			if self.email == current_app.config['FLASK_BLOG_ADMIN']:
+				self.role = Role.query.filter_by(permissions=0xff).first()
+			if self.role is None:
+				self.role = Role.query.filter_by(default=True).first()
+
+	def can(self, permissions):
+		return self.role is not None and (self.role.permissions & permissions) == permissions
+
+	def is_administrator(self):
+		return self.can(Permissions.ADMINISTER)
 
 	@property
 	def password(self):
@@ -56,6 +96,17 @@ class User(UserMixin, db.Model):
 		self.confirmed = True
 		db.session.add(self)
 		return True
+
+
+class AnonymousUser(AnonymousUserMixin):
+	def can(self, permissions):
+		return False
+
+	def is_administrator(self):
+		return False
+
+
+login_manager.anonymous_user = AnonymousUser
 
 
 @login_manager.user_loader
